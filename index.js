ff4c3dbf5926098cbf7fbc8a7b0b28aa458d3fed
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2023-12-20 21:39:22 +0530

    cli first version

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`Mahesh Guntumadugu 
+
+Senior Engineer.Coding enthusiast with strong development skills,
+eager to learn new technologies, and experienced as a freelancer
+
+
+
+Medium: https://medium.com/@maheshguntumadugu
+Github: https://github.com/gmaheshraju
+LinkedIn: https://www.linkedin.com/in/gmaheshraju/
+
+`);
