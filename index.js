279e6c0b4a188d8365fd9477bb940b75d3ac02a6
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2023-12-23 12:59:32 +0530

    added new changes for cleareing console

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+const clearConsole = require("clear-any-console");
+
+// Clears the console.
+clearConsole();
 console.log(`Mahesh Guntumadugu 
 
 Senior Engineer.Coding enthusiast with strong development skills,
